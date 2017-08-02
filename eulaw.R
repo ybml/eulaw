d5c311cd0416ad09bfdb7947a03ea8624ed45f19
John Körtner <jkortner@protonmail.com>
2017-08-02 13:31:53 +0200

    Create eulaw.R

---
+++ eulaw.R
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------- #
+Treaties of the European Union
+# --------------------------------------------------------------------------- #
+
+# ECSC
+
+# EURATOM (Treaty of Rome)
+
+# EEC (Treaty of Rome)
+
+# Merger Treaty
+
+# SEA
+
+# TEU
+
+# Amsterdam Treaty
+
+# Treaty of Nice
+
+# Treaty of Lisbon
