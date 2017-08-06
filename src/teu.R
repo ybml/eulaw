8aa1c7e5327251d701c825de8cbd1cf8ba697dd7
John Körtner <jkortner@pm.me>
2017-08-06 13:07:24 +0200

    teu (article numbering needs to be checked and changed, wikisource seems wrong)

--- src/teu.R
+++ src/teu.R
@@ -2,145 +2,77 @@
 # Treaty on European Union (Maastricht)
 # --------------------------------------------------------------------------- #
 
-ma_url <- "https://en.wikisource.org/wiki/Treaty_on_European_Union"
-ma <- read_html(ma_url)
-write_html(ma, file = "src/htmls/ma.html")
+# August 6, 2017
+url = "https://en.wikisource.org/wiki/Treaty_on_European_Union"
+links = c("/Title_I:_Common_Provisions",
+          "/Title_II:_Provisions_Amending_the_Treaty_establishing_the_European_Economic_Community_with_a_view_to_establishing_the_European_Community",
+          "/Title_III:_Provisions_Amending_the_Treaty_Establishing_the_European_Coal_and_Steel_Community",
+          "/Title_IV:_Provisions_Amending_the_Treaty_Establishing_the_European_Atomic_Energy_Community",
+          "/Title_V:_Provisions_on_a_Common_Foreign_and_Security_Policy",
+          "/Title_VI:_Provisions_on_Cooperation_in_the_Fields_of_Justice_and_Home_Affairs",
+          "/Title_VII:_Final_Provisions")
 
+# scrape 
+for (i in 1:length(links)) { 
 
-# 1. Get Links
-# get all links inside a <li>
-links <- ma %>% 
-  html_nodes("li>a") %>% 
-  html_attr("href")
-
-links <- paste0("https://en.wikisource.org", links)
-rm(ma)
-
-# preamble
-preamble_link <- links[str_detect(links, "/wiki/Treaty_on_European_Union")
-                       & str_detect(links, "Preamble")]
-
-# only those that refer to /wiki/Treaty_on_European_Union and Provisions
-prov_links <- links[str_detect(links, "/wiki/Treaty_on_European_Union")
-                    & str_detect(links, "Provisions")]
-rm(links)
-
-# 2. Extract 
-# 2.1. preamble
-preamble_h <- read_html(preamble_link)
-
-# in the preamble, all the p's form the text
-preamble <- preamble_h %>% 
-  html_nodes("p") %>% 
-  html_text()
-
-preamble <- data.frame(toctext = "Preamble", title = "Preamble",
-                       article = "0", paragraph = NA, list_elem = NA, 
-                       text_id = 1:length(preamble), text = preamble,
-                       stringsAsFactors = F)
-
-
-preamble_df <- preamble %>% 
-  select(toctext, title, article, paragraph, list_elem, text_id, text)
-
-# create id variable
-preamble_df <- preamble_df %>% 
-  unite(id, article:list_elem, sep = ".", remove = F)
-preamble_df$id <- str_replace_all(preamble_df$id, "NA", "x")
-
-# reorder the columns 
-preamble_df <- preamble_df %>% 
-  select(id, everything())
-
-
-rm(preamble_h, preamble_link, preamble)
-
-# 2.2. Provisions
-# prepare the htmls -> introduce article tags
-htmls <- lapply(prov_links, function(l){
-  h <- read_html(l)
-  h <- str_replace_all(h, "<h3>[\n]?<span", "</article> <article><h3><span")
-  h <- str_replace(h, "</article>", "") # first closing tag has no beginning tag
-  h <- read_html(h)
-  return(h)
-})
-
-results <- lapply(htmls, function(h){
-  # get the title 
-  title <- h %>% 
-    html_node("span#header_section_text") %>% 
+  html = read_html(paste0(url, links[i]))
+  
+  treaty = html %>%
+    html_nodes("h3,p,dl") %>%
     html_text()
   
-  # still the amendments are contained within articles
-  # get article id 
-  ids <- h %>% 
-    html_nodes("h3>span.mw-headline") %>% 
-    html_attr("id")
+  df = data.frame(text = treaty)
   
-  arts_text <- h %>% 
-    html_nodes("h3>span.mw-headline") %>% 
-    html_text() %>% 
-    str_replace("Article ", "")
+  df$index = seq_len(nrow(df))
+  df$title = as.character(i)
   
-  # construct a data frame
-  toc <- data.frame(title = title, a_id = ids, article = arts_text, 
-                    stringsAsFactors = F)
-  toc$article_num <- as.character(1:nrow(toc))
+  df$text = str_replace_all(df$text, "\\[edit\\]", " ")
   
-  articles <- get_nested_text(h, "article", "article_num", "p,li,dd")
-  articles <- left_join(articles, toc, by = "article_num")
-  articles <- extract_paragraphs(articles, regex = "^\\d+\\.")
-  articles <- extract_list_elem(articles)
+  # delete rows title by title
+  if (links[i] == links[1]) {
+    df = df[order(df$index),] # check
+    df = subset(df, df$index >= 3 & df$index <= 27)
+    teu = df
+  }
   
-  return(articles)
-})
+  if (links[i] == links[2]) {
+    df = df[order(df$index),] # check
+    # df = subset(df, df$index <= 989)
+    teu = bind_rows(teu, df)
+  }
+  
+  if (links[i] == links[3]) {
+    df = df[order(df$index),] # check
+    df = subset(df, df$index <= 170)
+    teu = bind_rows(teu, df)
+  }
+  
+  if (links[i] == links[4]) {
+    df = df[order(df$index),] # check
+    df = subset(df, df$index <= 203)
+    teu = bind_rows(teu, df)
+  }
+  
+  if (links[i] == links[5]) {
+    df = df[order(df$index),] # check
+    df = subset(df, df$index >= 3 & df$index <= 69)
+    teu = bind_rows(teu, df)
+  }
+  
+  if (links[i] == links[6]) {
+    df = df[order(df$index),] # check
+    df = subset(df, df$index <= 54)
+    teu = bind_rows(teu, df)
+  }
+  
+  if (links[i] == links[7]) {
+    df = df[order(df$index),] # check
+    df = subset(df, df$index >= 3 & df$index <= 29)
+    teu = bind_rows(teu, df)
+  }
+}
+rm(df, links, url)
 
-provisions <- bind_rows(results)
+# get articles -------------------------------------------------------------- #
 
-# arrange
-provisions <- provisions %>% 
-  arrange(title, article, text_id)
-
-# delete article num and a_id
-provisions$article_num <- NULL
-provisions$a_id <- NULL
-
-# add variables that are not yet part of the data frame
-# provisions$tocnumber <- NA
-provisions$toctext <- provisions$title # in this case, this is the same
-# provisions$chapter <- NA
-# provisions$section <- NA
-# provisions$part <- NA
-
-# extract title number 
-provisions$title <- unlist(str_replace_all(provisions$title, 
-                                           "Title (.+?)\\:.+", "\\1"))
-
-# article name includes "." -> remove them in order not to mess with the id later
-provisions$article <- str_replace_all(provisions$article, "\\.", "")
-
-provisions_df <- provisions %>% 
-  select(toctext, title, article, paragraph, list_elem, text_id, text)
-
-# create id variable
-provisions_df <- provisions_df %>% 
-  unite(id, title:list_elem, sep = ".", remove = F)
-provisions_df$id <- str_replace_all(provisions_df$id, "NA", "x")
-
-# reorder columns
-provisions_df <- provisions_df %>% 
-  select(id, everything())
-
-# 3. Combine and export 
-# bind together with preamble
-maastricht <- bind_rows(preamble_df, provisions_df)
-
-maastricht <- maastricht %>% 
-  mutate(treaty = "maastricht") %>% 
-  mutate(in_db = !str_detect(toctext, "[Aa]mending|Preamble")) %>% 
-  select(treaty, everything())
-
-# give text id anew because it is for now nested within the titles / preamble
-maastricht$text_id <- 1:nrow(maastricht)
-
-write.csv(maastricht, file = "data/maastricht.csv", row.names = F)
+# ...
