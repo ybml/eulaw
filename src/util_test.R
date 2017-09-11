547eb27faf333868f75c5d079e8ba6aa4da9d694
rogerrabbig <fgante@localhost.localdomain>
2017-09-11 14:00:42 +0200

    Minimal working example to test changes functions.

---
+++ src/util_test.R
@@ -0,0 +1,12 @@
+# Test the functions implementing the changes. 
+
+# Libraries --------------------------------------------------------------------
+library(tidyverse)
+
+# Workspace preparation --------------------------------------------------------
+rm(list = ls())
+
+# Functions and data
+source("src/utils.R")
+load("tables/mwe.Rdata")
+
