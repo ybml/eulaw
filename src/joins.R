d410a9024995f2451cd1594858d848de5832ea92
rogerrabbig <fgante@localhost.localdomain>
2017-09-17 13:30:33 +0200

    Fix of the NA problem

--- src/joins.R
+++ src/joins.R
@@ -16,8 +16,10 @@ eulaw_1992 <- readRDS("data/eulaw_1992.rds")
 eulaw <- full_join(eulaw_1951, eulaw_1957, by = c("id_1951" = "id_1951")) %>%
   select(id_1951, id_1957, txt_1951 = txt.x, txt_1957 = txt.y) %>%
   full_join(eulaw_1965, by = c("id_1957" = "id_1957")) %>%
+  mutate(id_1965 = if_else(is.na(id_1965), "repealed", id_1965)) %>%
   select(id_1951, id_1957, id_1965, txt_1951, txt_1957, txt_1965 = txt) %>%
   full_join(eulaw_1986, by = c("id_1965" = "id_1965")) %>%
+  mutate(id_1986 = if_else(is.na(id_1986), "repealed", id_1986)) %>%
   select(id_1951,
     id_1957,
     id_1965,
@@ -36,6 +38,9 @@ eulaw <- full_join(eulaw_1951, eulaw_1957, by = c("id_1951" = "id_1951")) %>%
     txt_1957,
     txt_1965,
     txt_1986,
-    txt_1992 = txt)
+    txt_1992 = txt) %>%
+  mutate_at(vars(contains("id")),
+            funs(if_else(. == "repealed", NA_character_, .))
+  )
 
 saveRDS(eulaw, file = "data/eulaw.rds")
