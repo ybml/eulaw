22999a45276193c52f4a26620a9d4b9554c57a12
rogerrabbig <fgante@localhost.localdomain>
2017-09-18 10:33:36 +0200

    datatable commands

--- src/presentation.R
+++ src/presentation.R
@@ -12,17 +12,19 @@ euratom <- read_csv("tables/euratom.csv")
 
 # Treaty Example
 # Euratom has title, chapter, section, article.
-datatable(euratom)
+datatable(select(euratom, -treaty), width = 1500,
+          options = list(autoWidth = TRUE, pageLength = nrow(euratom),
+          columnDefs = list(list(width = '600px', targets = c(6)))))
 
 # Changes example
 teu_changes <- teu_changes %>%
   filter(action == "replace_txt_globally")
 changes_example <- bind_rows(merger_changes, teu_changes)
-datatable(changes_example)
+datatable(changes_example, width = 1500)
 
 # Final data
-datatable(eulaw)
+datatable(eulaw, width = 1500,
+          options = list(autoWidth = TRUE, pageLength = nrow(eulaw)))
 
-# Visualization example
 
 #EOF
